5e01761429094816c3e99a07cb9f4ec40b52e983
yeonjoo_Kim <vanilla05284896@gmail.com>
2023-04-11 13:14:14 +0900

    enemypool
    EnemyPool 생성

--- Assets/Script/Spawner&Pool/Enemies/Pool/BoxboxerPool.cs
+++ Assets/Script/Spawner&Pool/Enemies/Pool/BoxboxerPool.cs
@@ -2,6 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyPool : ObjectPool<Enemy_Boxboxer>
+public class BoxboxerPool : ObjectPool<Enemy_Boxboxer>
 {
 }
